aac098ba55c3a961565fca3ecde46652e9fc1885
Vladimir Tkach <teamco@gmail.com>
2021-04-22 02:13:57 +0300

    add external models

--- .umirc.js
+++ .umirc.js
@@ -2,18 +2,46 @@ import { defineConfig } from 'umi';
 import { routes } from './routes';
 import proxy from './proxy';
 
+const fs = require('fs');
+
+/**
+ * @function
+ * @param dir
+ * @param files_
+ * @return {*[]}
+ */
+function getFiles(dir, files_) {
+  files_ = files_ || [];
+  const files = fs.readdirSync(dir);
+  for (let i in files) {
+    const name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files_);
+    } else {
+      if (name.match(/.model/)) {
+        files_.push(name);
+      }
+    }
+  }
+  return files_;
+}
+
+const widgetsPath = `${__dirname}/src/vendors/widgets`;
+const extraModels = getFiles(widgetsPath);
+
 export default defineConfig({
   crossorigin: true,
   routes,
   proxy,
   dva: {
+    extraModels,
     immer: true,
-    hmr: true,
+    hmr: true
   },
   lessLoader: {
-    lessLoaderOptions: {},
+    lessLoaderOptions: {}
   },
   nodeModulesTransform: {
-    type: 'none',
-  },
+    type: 'none'
+  }
 });
